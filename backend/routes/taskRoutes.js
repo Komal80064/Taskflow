f89f9981f7dc2b35be1e1f52b5274cdfc5fdc277
Komal <panwarkomal2003@gmail.com>
2026-09-11 17:37:58 +0530

    update taskcontroller

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -6,7 +6,7 @@ const {
   createTask,
   updateTask,
   deleteTask,
-} = require("../controllers/taskController");
+} = require("../controllers/taskcontroller");
 
 const protect = require("../middleware/authMiddleware");
 
