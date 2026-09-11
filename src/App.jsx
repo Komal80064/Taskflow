b0b404e277a06625e9fe585527ba0c19c70f1e29
Komal <panwarkomal2003@gmail.com>
2026-09-11 17:58:48 +0530

    Updated App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import "./app.css";
+import "./App.css";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Today from "./pages/Today/Today";
